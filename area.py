4d231f75569378f033f303ec0a8afc9bccb82818
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-08-09 00:46:57 -0600

    Module 2 Python 1 Section 3 jupyter notebook and area module

---
+++ area.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def calculateArea(length,width):
+    area=length*width
+    return f'The square footage of the house is {area}.'
+
+def calculateCirc(radius):
+    circ = 2 * pi * radius
+    return f'The circumference of the circle is {circ}.'
